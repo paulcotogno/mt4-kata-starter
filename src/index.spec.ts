89a8435296602286dc7a749c510b233a30fb86b4
paulcotogno <71767507+paulcotogno@users.noreply.github.com>
2023-06-12 15:40:20 +0200

    Add test and algo

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,38 +1,92 @@
-// @ts-ignore see https://github.com/jest-community/jest-extended#setup
+// @ts-ignore see https://gtesthub.com/jest-communtesty/jest-extended#setup
 import * as matchers from "jest-extended";
-import fc from "fast-check";
-import { main } from ".";
+import { runRPN } from ".";
 
 expect.extend(matchers);
 
-test('call the main', () => {
-  expect(main()).toEqual('coucou');
+test("Test soustraction", () => {
+  expect(runRPN("1 1 -")).toEqual(0);
+});
+
+test("Test addition", () => {
+  expect(runRPN("1 2 +")).toEqual(3);
+});
+
+test('Test multiplication', () => {
+  expect(runRPN('3 2 *')).toEqual(6);
 })
 
-/* test("A simple test (Jest)", () => {
-  expect(1 + 1).toEqual(2);
+test('Test division', () => {
+  expect(runRPN('3 2 *')).toEqual(6);
+})
+
+test("Test negate", function () {
+  expect(runRPN("2 NEGATE 6 +")).toEqual(4);
 });
 
-test("Additional matchers (jest-extended)", () => {
-  expect([1, 0]).toIncludeSameMembers([0, 1]);
+test("Test modulo", function () {
+  expect(runRPN("7 3 MOD")).toEqual(1);
 });
 
-test("Property-based testing (fast-check)", () => {
-  type Boundaries = {
-    min: number;
-    max: number;
-  };
-
-  const minmax =
-    ({ min, max }: Boundaries) =>
-    (n: number): number =>
-      Math.min(max, Math.max(min, n));
-
-  fc.assert(
-    fc.property(fc.integer(), (n): boolean => {
-      const result = minmax({ min: 1, max: 10 })(n);
-      return 1 <= result && result <= 10;
-    })
-  );
+test("Test negate failed", function () {
+  expect(() => runRPN("-2 6 +")).toThrow(TypeError);
+  expect(() => runRPN("-2 6 +")).toThrow("Bad value");
 });
- */
+
+test("Addition sup", function () {
+  expect(runRPN("99 11 + 8 7 + +")).toEqual(125);
+});
+
+test("Multiplication sup", function () {
+  expect(runRPN("4 7 * 5 2 * *")).toEqual(280);
+});
+
+test("Suppression sup", function () {
+  expect(runRPN("33 3 - 10 6 - -")).toEqual(26);
+});
+
+test("Division sup", function () {
+  expect(runRPN("90 3 / 30 5 / /")).toEqual(5);
+});
+
+test("All calcul at once", function () {
+  expect(runRPN("15 7 1 1 + - / 3 * 2 1 1 + + -")).toEqual(5);
+});
+
+test("Multiple operations", function () {
+  expect(runRPN("2")).toEqual(2);
+  expect(runRPN("3 4 +")).toEqual(7);
+  expect(runRPN("12 4 / 1 -")).toEqual(2);
+  expect(runRPN("12 4 1 - /")).toEqual(4);
+  expect(runRPN("15 7 1 1 + - / 3 * 2 1 1 + + -")).toEqual(5);
+});
+
+test("Bad operator", () => {
+  expect(() => runRPN("2 4 &")).toThrow(TypeError);
+  expect(() => runRPN("2 4 &")).toThrow("Bad operator");
+})
+
+test("Wrong number of operator and value", () => {
+  expect(() => runRPN('2 3 4 +')).toThrow(TypeError);
+  expect(() => runRPN('2 3 4 +')).toThrow("Stack too long");
+})
+
+test("Bad use of NEGATE", () => {
+  expect(() => runRPN("NEGATE 2")).toThrow(TypeError);
+  expect(() => runRPN("NEGATE 2")).toThrow("No value for NEGATE");
+})
+
+test("No value wtesth an operator", () => {
+  expect(() => runRPN("2 + 3")).toThrow(TypeError);
+  expect(() => runRPN("2 + 3")).toThrow("No value for operator");
+})
+
+test("Divide by 0", () => {
+  expect(() => runRPN("3 0 /")).toThrow(TypeError);
+  expect(() => runRPN("3 0 /")).toThrow("Cannot divide by 0");
+})
+
+test("Modulo by 0", () => {
+  expect(() => runRPN("3 0 MOD")).toThrow(TypeError);
+  expect(() => runRPN("3 0 MOD")).toThrow("Cannot MOD by 0");
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,53 @@
-export const main: () => string = () => {
-  return 'coucou';
+export const runRPN = (expr: string) => {
+  let exprList = expr.split(' ');
+
+  let stack: number[] = [];
+
+  for (let item of exprList) {
+    if (!isNaN(Number(item))) {
+      if (Number(item) < 0) throw new TypeError("Bad value");
+
+      stack.push(Number(item));
+    } else if (item === 'NEGATE') {
+      let a = stack.pop();
+
+      if (!a) throw new TypeError("No value for NEGATE");
+
+      stack.push(a * -1);
+    } else {
+      let operand = item,
+        b = stack.pop(),
+        a = stack.pop();
+
+      if (isNaN(a) || isNaN(b)) throw new TypeError("No value for operator")
+
+      switch (operand) {
+        case '+':
+          stack.push(a + b);
+          break;
+        case '-':
+          stack.push(a - b);
+          break;
+        case '*':
+          stack.push(a * b);
+          break;
+        case '/':
+          if (b === 0) throw new TypeError("Cannot divide by 0");
+          stack.push(a / b);
+          break;
+        case 'MOD':
+          if (b === 0) throw new TypeError("Cannot MOD by 0");
+          stack.push(a % b);
+          break;
+        default:
+          throw new TypeError("Bad operator");
+      }
+    }
+  }
+
+  if (stack.length > 1) {
+    throw new TypeError("Stack too long");
+  } else {
+    return stack[0];
+  }
 }
